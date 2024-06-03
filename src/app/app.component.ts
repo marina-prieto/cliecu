2730812310d69bebc6338136aa55c51b82065b7d
Marina Prieto <marina.prieto1@alu.uclm.es>
2024-06-03 20:10:51 +0200

    black mode+menubar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 import { EcuacionesComponent } from './ecuaciones/ecuaciones.component';
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, EcuacionesComponent, LoginComponent, RegisterComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterModule,
+    EcuacionesComponent,
+    LoginComponent,
+    RegisterComponent,
+    MatToolbarModule,
+    MatButtonModule
+  ]
 })
+
 export class AppComponent {
   title = 'cliecu';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { EcuacionesComponent } from './ecuaciones/ecuaciones.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'ecuaciones', component: EcuacionesComponent },
+  { path: '', redirectTo: '/register', pathMatch: 'full' }
+];
